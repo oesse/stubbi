6fdf973bbdd0b44e6e71305ec904590b781f2eb0
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-02-16 20:59:44 +0100

    feat: add schema for create-stubs route

--- src/endpoints/create-stubs.js
+++ src/endpoints/create-stubs.js
@@ -1,15 +1,41 @@
-export default (req, res) => {
-  const {
-    method, path, respondsWith, notifies,
-  } = req.body;
+import validateSchema from './validate-schema';
+
+export default [
+  validateSchema({
+    properties: {
+      method: {
+        type: 'string',
+        enum: ['get', 'post', 'put', 'patch', 'delete'],
+      },
+      path: { type: 'string' },
+      respondsWith: {
+        properties: {
+          headers: { type: 'object' },
+          status: { type: 'integer' },
+          body: {
+            anyOf: [{ type: 'string' }, { type: 'object' }],
+          },
+        },
+        additionalProperties: false,
+      },
+      notifies: { type: 'object' },
+    },
+    required: ['method', 'path'],
+    additionalProperties: false,
+  }),
+  (req, res) => {
+    const {
+      method, path, respondsWith, notifies,
+    } = req.body;
 
 
-  const { id } = req.stubs.createStub({
-    method, path, respondsWith, notifies,
-  });
+    const { id } = req.stubs.createStub({
+      method, path, respondsWith, notifies,
+    });
 
-  res.status(201);
-  res.json({
-    id, method, path, respondsWith, notifies,
-  });
-};
+    res.status(201);
+    res.json({
+      id, method, path, respondsWith, notifies,
+    });
+  },
+];
