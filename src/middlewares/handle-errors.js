6c301e0178e4fcc317b1b9418e499010d5279ba9
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-02-18 09:25:41 +0100

    feat: return bad request on schema validation failure

--- src/middlewares/handle-errors.js
+++ src/middlewares/handle-errors.js
@@ -1,7 +1,13 @@
+import { SchemaValidationFailed } from '../errors';
+
 // Expressjs needs `next` as fourth parameter to identify this middleware as
 // an error handling middleware.
 // eslint-disable-next-line no-unused-vars
 export default (err, req, res, next) => {
+  if (err instanceof SchemaValidationFailed) {
+    res.status(400).json({ errors: err.errors });
+    return;
+  }
   // eslint-disable-next-line no-console
   console.error(err);
   res.sendStatus(500);
